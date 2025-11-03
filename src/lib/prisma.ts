0f010883ed4101349271ffc85388e4234be957a9
Elecmonkey <elecmonkey@hotmail.com>
2025-11-03 23:24:30 +0800

    chore: pure js mode for prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,23 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-export const prisma = global.prisma ?? new PrismaClient();
+const pgPool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pgPool);
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+  globalForPrisma.prisma = prisma;
 }
